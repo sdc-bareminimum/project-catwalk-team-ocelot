219ee67f9d4484897f8f8307be80a430f31f5d6c
danpolitis <danpolitis@gmail.com>
2021-09-13 12:36:22 -0700

    Got list of related items into state of RelatedItems component

--- client/src/components/RelatedItems.jsx
+++ client/src/components/RelatedItems.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function RelatedItems() {
+function RelatedItems({ productId, setProductId }) {
+  const [relatedList, setRelatedList] = useState([]);
+
+  const getRelatedItems = (id) => {
+    axios.get(`/api/products/${id}/related`)
+      .then(({ data }) => {
+        setRelatedList(data);
+      });
+  };
+
+  useEffect(() => {
+    getRelatedItems(productId);
+  }, []);
+
   return <div>RelatedItems</div>;
 }
 

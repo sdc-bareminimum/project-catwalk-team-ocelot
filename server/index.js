60ea8a1fb5aad281f80ae72c05a5eb9487480d85
danpolitis <danpolitis@gmail.com>
2021-09-15 11:49:02 -0700

    Removed attempted fix adding access control allow origin to server

--- server/index.js
+++ server/index.js
@@ -25,7 +25,6 @@ app.use('/api/*', (req, res) => {
     headers: {
       'User-Agent': 'request',
       'Authorization': config.TOKEN,
-      'Access-Control-Allow-Origin': 'http://localhost:3000',
     },
     data: body,
   };

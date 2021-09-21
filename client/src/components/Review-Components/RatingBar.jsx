c39a208afb84312da55af7805cff0ccf1565e596
Andrew Hang <Andrewhang94@gmail.com>
2021-09-21 01:57:15 -0700

    added filter by star feature and added test coverage for review components

--- client/src/components/Review-Components/RatingBar.jsx
+++ client/src/components/Review-Components/RatingBar.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import uniqid from 'uniqid';
 
-const RatingBar = ({ total, ratings }) => {
+const RatingBar = ({
+  total, ratings, handleChange, starFilter,
+}) => {
   const arrayHolder = [5, 4, 3, 2, 1];
+  const [colorClick, setcolorClick] = useState(false);
   const progressBar = arrayHolder.map((star) => {
     const toNumberStar = Number(ratings[star]) || 0;
     const rating = toNumberStar ? (toNumberStar / total) * 100 : 0;
     return (
       <div key={uniqid()}>
         <p className="progress-label">
-          <u>
+          <input className="check-box-rating" type="checkbox" checked={starFilter[star]} value={star.toString()} onChange={handleChange} />
+          <u style={starFilter[star] ? { color: 'green' } : { color: 'black' }}>
             {star}
             {' '}
             Stars

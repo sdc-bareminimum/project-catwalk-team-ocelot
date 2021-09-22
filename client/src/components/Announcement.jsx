1e2f292fd3a6e8051f0093b6e5a83ad70a4af2a3
Andrew Hang <Andrewhang94@gmail.com>
2021-09-22 01:32:45 -0700

    added announcement component

---
+++ client/src/components/Announcement.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Announcement() {
+  return (
+    <div id="announcement">
+      <p className="text-center">
+        SITE WIDE ANNOUNCEMENT MESSAGE! SALE / DISCOUNT
+        {' '}
+        <b>OFFER</b>
+        {' '}
+        -
+        {' '}
+        <u>NEW PRODUCT HIGHLIGHT</u>
+      </p>
+    </div>
+  );
+}
+
+export default Announcement;

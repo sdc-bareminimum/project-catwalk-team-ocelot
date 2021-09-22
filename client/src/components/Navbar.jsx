96a1b1e03b4d92c6c2755f5c34949b74d13ec320
Andrew Hang <Andrewhang94@gmail.com>
2021-09-22 01:22:36 -0700

    added logo and changed navbar to black

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { FaSearch } from 'react-icons/fa';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-secondary px-5 py-3">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-5 py-3">
       <div className="container-fluid">
-        <a className="navbar-brand text-white" href="/">Logo</a>
+        <a className="navbar-brand text-white" href="/"><img src="./catpaw.png" alt="logo" width={50} height={50} /></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
